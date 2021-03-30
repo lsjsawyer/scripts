460346dfa3d14376833c8a6ba2742a1fdd3056ff
lsjsawyer <1159200202@qq.ccom>
2021-03-30 16:11:53 +0800

    加入yaml提高代码编写效率

--- script/test_ca.py
+++ script/test_ca.py
@@ -2,12 +2,26 @@
 # import sys
 # sys.path.append("/root/monitoring")
 import allure
-
+import pytest
 from base.get_driver import get_xiaomi_mix_8_no_session_driver
-from time import sleep
-from datetime import datetime
+from base.getfiledata import GetFileData
 from base.pages import LMPage
 from selenium.common.exceptions import TimeoutException
+
+def get_account():
+    suc_list = []
+    fail_list = []
+    account_data = GetFileData().get_yaml_data("test_account")
+    for i in account_data:
+        if (account_data.get(i)).get("merchants_data"):
+            fail_list.append((i, account_data.get(i).get("account"), account_data.get(i).get("passwd"),
+                              account_data.get(i).get("merchants_data")))
+        else:
+            suc_list.append((i, account_data.get(i).get("account"), account_data.get(i).get("passwd"),
+                             account_data.get(i).get("expect_data")))
+    return {"suc": suc_list, "fail": fail_list}
+
+
 
 @allure.feature("ca认证测试")
 class TestHuanShaouLv:
@@ -23,14 +37,15 @@ class TestHuanShaouLv:
     @allure.severity(allure.severity_level.CRITICAL)
     @allure.testcase("https://www.bugclose.com/testCase.html?id=3405&projectId=38335")
     @allure.step(title="预开户用户进入招商银行银证转账页面,弹出ca认证弹窗")
-    def test_01(self):
+    @pytest.mark.parametrize("case_num, account, passwd, expect_data", get_account().get("suc"))
+    def test_01(self, case_num, account, passwd, expect_data):
         self.page.market_page_lm().go_to_mine_page()
-        self.page.mine_page_lm().log_in_correct_account("10501642300", "lm123456")
+        self.page.mine_page_lm().log_in_correct_account(account, passwd)
         self.page.turn_to_money_page_lm().go_to_mainland_bank_card_page()
         try:
             ca_pop_result = self.page.turn_to_money_page_lm().get_ca_pop_result()
             try:
-                assert "请您进行客户身份信息安全认证" in ca_pop_result
+                assert expect_data in ca_pop_result
                 self.page.turn_to_money_page_lm().from_yinzheng_moneyin_back_to_mine_page()
             except AssertionError:
                 self.page.turn_to_money_page_lm().screen_shot("判定错误截图")
@@ -69,14 +84,15 @@ class TestHuanShaouLv:
     @allure.story("ca弹窗测试")
     @allure.testcase("https://www.bugclose.com/testCase.html?id=3405&projectId=38335")
     @allure.step(title="已开户用户点击银证转账银行,进入该银行详细信息页面")
-    def test_03(self):
+    @pytest.mark.parametrize("case_num, account, passwd, merchants_data", get_account().get("fail"))
+    def test_03(self, case_num, account, passwd, merchants_data):
         self.page.market_page_lm().go_to_mine_page()
-        self.page.mine_page_lm().log_in_correct_account("10020680000", "lm123456")
+        self.page.mine_page_lm().log_in_correct_account(account, passwd)
         self.page.turn_to_money_page_lm().go_to_mainland_bank_card_page()
         try:
             merchants_bank_page_result = self.page.turn_to_money_page_lm().get_merchants_bank_details()
             try:
-                assert "需先在香港招商网银申请开通" in merchants_bank_page_result
+                assert merchants_data in merchants_bank_page_result
                 self.page.turn_to_money_page_lm().from_merchants_bank_details_page_back_to_mine_page()
             except AssertionError:
                 self.page.turn_to_money_page_lm().screen_shot("判定错误截图")
